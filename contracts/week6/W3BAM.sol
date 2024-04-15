56f5cb6fa92f7d8ff9b21c6ba285cf6fdfdfa96e
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2024-04-16 01:47:21 +0200

    Name and ticker fix.

--- contracts/week6/W3BAM.sol
+++ contracts/week6/W3BAM.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract W3BAM is ERC20, Ownable(msg.sender) {
-    constructor() ERC20("W3BAM", "Web3 BAM") {
+    constructor() ERC20("Web3 BAM", "W3BAM") {
         _mint(msg.sender, 100000 * 10 ** 18);
     }
 
--- contracts/week6/W3Euro.sol
+++ contracts/week6/W3Euro.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract W3EUR is ERC20, Ownable(msg.sender) {
-    constructor() ERC20("W3EUR", "Web3 Euro") {
+    constructor() ERC20("Web3 Euro", "W3EUR") {
         _mint(msg.sender, 100000 * 10 ** 18);
     }
 
